69b2c00e56252a032dc92792eb1a3370fa7eb833
friendlysxw <friendlysxw@163.com>
2018-11-27 00:46:15 +0800

    Site updated: 2018-11-27 00:46:13

--- dist/music.js
+++ dist/music.js
@@ -10,22 +10,30 @@ const ap = new APlayer({
     mutex: true, //互斥，阻止多个播放器同时播放，当前播放器播放时暂停其他播放器
     listFolded: false, //列表默认折叠
     listMaxHeight: 90, //列表最大高度
-    lrcType: 3, //歌词传递方式
+    lrcType: 2, //歌词传递方式
     audio: [ //音频信息,包含以下
         {
-            name: 'name1', //音频名称
-            artist: 'artist1', //音频艺术家
-            url: 'url1.mp3', //音频外链
-            cover: 'cover1.jpg', //音频封面
-            lrc: 'lrc1.lrc', //音频歌词，配合上面的lrcType使用
+            name: '鹿港小镇', //音频名称
+            artist: '彭佳慧', //音频艺术家
+            url: 'http://music.163.com/song/media/outer/url?id=27583489.mp3', //音频外链
+            cover: 'http://p2.music.126.net/RLrxpILv9Tpq2rZrqlPDmw==/6661940952793940.jpg?param=130y130', //音频封面
+            // lrc: './dist/lrc/a.lrc', //音频歌词，配合上面的lrcType使用
             theme: '#ebd0c2' //切换到此音频时的主题色，比上面的 theme 优先级高
         },
         {
-            name: 'name2', //如果只有一首歌，删掉这一块，如有更多歌曲按此格式逐渐往下添加
-            artist: 'artist2',
-            url: 'url2.mp3',
-            cover: 'cover2.jpg',
-            lrc: 'lrc2.lrc',
+            name: '尽头', //如果只有一首歌，删掉这一块，如有更多歌曲按此格式逐渐往下添加
+            artist: '苏浩',
+            url: 'http://music.163.com/song/media/outer/url?id=438466179.mp3',
+            cover: 'http://p1.music.126.net/jaFuzydptk97fcE8OsKs1Q==/1407374900136692.jpg?param=130y130',
+            // lrc: 'lrc2.lrc',
+            theme: '#46718b'
+        },
+        {
+            name: '给女儿的一封信', //如果只有一首歌，删掉这一块，如有更多歌曲按此格式逐渐往下添加
+            artist: '张杰',
+            url: 'http://music.163.com/song/media/outer/url?id=561307188.mp3',
+            cover: 'http://p1.music.126.net/OOe6AHsW3aMRSfgLyTEQNw==/109951163298710803.jpg?param=130y130',
+            // lrc: 'lrc2.lrc',
             theme: '#46718b'
         }
     ]
